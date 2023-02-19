6b65916f280c4987abebff1f8d4bd0a3a7fb2507
Lucas Augusto <lucasaugustscode@gmail.com>
2023-02-18 22:08:27 -0300

    chore: Version with node settings

--- node.js
+++ node.js
@@ -4,15 +4,15 @@ module.exports = {
     project: './tsconfig.json'
   },
   rules: {
-    "@typescript-eslint/strict-boolean-expressions": 'off',
-    "@typescript-eslint/no-misused-promises": 'off',
-    "@typescript-eslint/no-namespace": 'off',
-    "@typescript-eslint/no-throw-literal": 'off',
-    "@typescript-eslint/no-dupe-class-members": 'off',
-    "@typescript-eslint/adjacent-overload-signatures": 'off',
-    "@typescript-eslint/naming-convention": 'off',
-    "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/prefer-nullish-coalescing": 'off',
-    "@typescript-eslint/no-extraneous-class": 'off'
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/no-dupe-class-members': 'off',
+    '@typescript-eslint/adjacent-overload-signatures': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off'
   }
 }
